099e853a3a06b2dfc1f8dd6f21bca23e95074adc
YurasovDV <yurasovdv@gmail.com>
2021-08-30 21:56:25 +0300

    extracted inline styles

--- src/components/repetition/repetition.js
+++ src/components/repetition/repetition.js
@@ -9,6 +9,8 @@ import {
 
 import * as constants from "../../constants";
 
+import './repetition.css';
+
 class Repetition extends Component {
   componentDidMount() {
     this.props.createRepetitionSet();
@@ -31,37 +33,25 @@ class Repetition extends Component {
 
     const w = words[0];
     return (
-      <div
-        className="bg-light"
-        style={{ width: "100%", height: "100%", minHeight: "800px" }}
-      >
+      <div className="bg-light w-100-h-100-minh600">
         <div className="container">
-          <div
-            className="row justify-content-around"
-            style={{ marginTop: "150px" }}
-          >
-            <div
-              className="col-md-auto rounded-pill bg-primary d-flex justify-content-center"
-              style={{ height: "150px", minWidth: "400px" }}
-            >
-              <h1 className="align-self-center text-light" style={{ overflow: "hidden" }}>{w.term}</h1>
+          <div className="row justify-content-around mt-150">
+            <div className="col-md-auto rounded-pill bg-primary d-flex justify-content-center w-400-h-150-mt-70">
+              <h1 className="align-self-center text-light overflow-hidden">
+                {w.term}
+              </h1>
             </div>
           </div>
-          <div
-            className="row justify-content-around"
-            style={{ marginTop: "150px" }}
-          >
-            <div
-              className="col-4 rounded-pill bg-success d-flex justify-content-center"
-              style={{ height: "150px" }}
-            >
-              <h2 className="align-self-center text-light" style={{overflow: "hidden" }}>{w.translations[0]}</h2>
+          <div className="row justify-content-around mt-150">
+            <div className="col-4 rounded-pill bg-success d-flex justify-content-center h-150">
+              <h2 className="align-self-center text-light overflow-hidden">
+                {w.translations[0]}
+              </h2>
             </div>
-            <div
-              className="col-4 rounded-pill bg-success d-flex justify-content-center"
-              style={{ height: "150px" }}
-            >
-              <h2 className="align-self-center text-light" style={{ overflow: "hidden" }}>{w.option}</h2>
+            <div className="col-4 rounded-pill bg-success d-flex justify-content-center h-150">
+              <h2 className="align-self-center text-light overflow-hidden">
+                {w.option}
+              </h2>
             </div>
           </div>
         </div>

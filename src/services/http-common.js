de64d114f234a797c1687b4e16aeb205caa9d5d4
YurasovDV <yurasovdv@gmail.com>
2022-01-01 17:43:06 +0300

    fixed api url

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://192.168.0.3:82/api/v1",
+    baseURL: "http://192.168.0.6:82/api/v1",
     headers: {
         "Content-type" : "application/json"
     }
